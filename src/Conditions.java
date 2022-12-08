25b96d165f0c83c15eced2cad9c3b62b7861d666
Brillionb <welkommen@yandex.ru>
2022-12-08 19:32:34 +0300

    feat: string

--- src/Conditions.java
+++ src/Conditions.java
@@ -15,3 +15,99 @@ public class Conditions {
         }
     }
 }
+
+class NumberOfMonthWithIf{
+    public static void main(String[] args) {
+        // Задача - программа принимает номер месяца и выводит его название на экран
+        // Если введен несуществующий номер - выводим "месяц не существует"
+        // массивы не использовать
+
+        int number = 4;
+        String result = "";
+        if (number == 1){
+            result = "январь";
+        } else if (number == 2){
+            result = "февраль";
+        } else if (number == 3){
+            result = "март";
+        } else if (number == 4){
+            result = "апрель"; // и так далее
+        } else {
+            result = "месяц не существует";
+        }
+        System.out.println(result);
+    }
+}
+
+class SwitchCase {
+    public static void main(String[] args) {
+        int number = 17;
+        String result = "";
+        switch (number){
+            case 1 : result = "январь";
+            break;
+            case 2 : result = "февраль";
+                break;
+            case 3 : result = "март";
+                break;
+            case 4 : result = "апрель";
+                break;
+            case 5 : result = "май";
+                break;
+            case 6 : result = "июнь";
+                break;
+            case 7 : result = "июль";
+                break;
+            case 8 : result = "август";
+                break;
+            case 9 : result = "сентябрь";
+                break;
+            case 10 : result = "октябрь";
+                break;
+            case 11 : result = "ноябрь";
+                break;
+            case 12 : result = "декабрь";
+                break;
+            default: result = "месяц не существует";
+        }
+        System.out.println(result);
+    }
+}
+
+class TimeOfYear{
+    public static void main(String[] args) {
+        //программа принимает строковую переменную название месяца
+        //вывести время года, иначе - месяц не существует
+
+        String month = "абракадабра";
+        String result = "";
+        switch (month){
+          case  "январь" : result = "зима";
+          break;
+            case  "февраль" : result = "зима";
+                break;
+            case  "март" : result = "весна";
+                break;
+            case  "апрель" : result = "весна";
+                break;
+            case  "май" : result = "весна";
+                break;
+            case  "июнь" : result = "лето";
+                break;
+            case  "июль" : result = "лето";
+                break;
+            case  "август" : result = "лето";
+                break;
+                case  "сентябрь" : result = "осень";
+                break;
+            case  "октябрь" : result = "осень";
+                break;
+            case  "ноябрь" : result = "осень";
+                break;
+            case  "декабрь" : result = "зима";
+                break;
+            default: result = "месяц не существует";
+        }
+        System.out.println(result);
+    }
+}
